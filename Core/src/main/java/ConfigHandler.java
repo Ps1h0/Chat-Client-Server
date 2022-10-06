481ce83f2b1871e5d043a7af844ed17b83b49a20
Ps1h0 <platonov.liceum@mail.ru>
2022-10-06 20:28:24 +0300

    Добавлены файлы конфигурации и их обработчик

---
+++ Core/src/main/java/ConfigHandler.java
@@ -0,0 +1,27 @@
+import lombok.AllArgsConstructor;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigHandler {
+
+    @AllArgsConstructor
+    public enum Name {
+        Client ("Client"),
+        Server ("Server");
+
+        private final String title;
+    }
+
+    public static Properties handleConfig(Name name) {
+        String configFilePath = name.title + "/src/main/resources/configuration.properties";
+        Properties prop = new Properties();
+        try (FileInputStream propsInput = new FileInputStream(configFilePath)) {
+            prop.load(propsInput);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+}
